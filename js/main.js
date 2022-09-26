5e43cd1abf28f09ea4e7e8a1871fb8c2faa6d024
HatakeSunflower <st1809149@upy.edu.mx>
2022-09-26 12:30:37 -0500

    xs

--- js/main.js
+++ js/main.js
@@ -1,19 +1,7 @@
-/* When the user clicks on the button, 
-    toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+var strings = ["BD", "55", "7A", "1C"];
+var randomIndex = Math.floor(Math.random() * strings.length);
+var randomString1 = strings[randomIndex];
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function (event) {
-  if (!event.target.matches(".dropbtn")) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
-      }
-    }
-  }
-};
+Array.from({ length: 36 }, () => Math.floor(Math.random() * strings.length));
+
+document.getElementById("f1c1").innerHTML = randomString1;
