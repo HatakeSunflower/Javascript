f4ad2720ebfac6e8237b0bf125e072bd670b1964
HatakeSunflower <st1809149@upy.edu.mx>
2022-10-10 23:01:09 -0500

    v1

---
+++ js/main copy.js
@@ -0,0 +1,9 @@
+alert("Bienvenido al cotizador de HatakeSNFLWR");
+let service = prompt("Dime el tipo de servicio: 'desarollo', 'diseño' o 'ilustracion'");
+const type = ['desarollo', 'diseño', 'ilustracion'];
+
+function development(){
+    if (service = type[0]) {
+        return alert(`Bienvenido`);
+    }
+}
